b0e961470fb682386866d321bb0d9a06dfa21ad8
BiliksuunSamuel <biliksuunsamuel@gmail.com>
2022-06-12 22:28:01 +0000

    make it great

--- src/renderer/App/components/InvoiceReceiptItemTable.tsx
+++ src/renderer/App/components/InvoiceReceiptItemTable.tsx
@@ -14,25 +14,30 @@ export default function InvoiceReceiptItemTable({ items }: IProps) {
       <thead>
         <tr>
           <th colSpan={2}>Item</th>
-          <th>Quantity</th>
-          <th>Unit Cost</th>
-          <th>Cost</th>
+          <th align="center">Quantity</th>
+          <th align="center">Unit Cost</th>
+          <th align="right">Cost</th>
         </tr>
       </thead>
       <tbody>
         {items.map((item) => (
           <tr>
             <td colSpan={2}>{item.title}</td>
-            <td>{item.quantity}</td>
-            <td>{currency + item.unit_cost}</td>
-            <td>{currency + item.quantity * item.unit_cost}</td>
+            <td align="center">{item.quantity}</td>
+            <td align="center">{currency + item.unit_cost}</td>
+            <td align="right" style={{ textAlign: "right" }}>
+              {currency + item.quantity * item.unit_cost}
+            </td>
           </tr>
         ))}
         <tr>
           <td style={{ fontWeight: "bolder" }} colSpan={4}>
             Total
           </td>
-          <td style={{ fontWeight: "bolder" }}>
+          <td
+            align="right"
+            style={{ fontWeight: "bolder", textAlign: "right" }}
+          >
             {currency + GetAmountDue(items)}
           </td>
         </tr>
